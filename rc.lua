13498dc03e7bcc1091958d69d7cfe996070a8962
umidk1837bss <umidk1837@gmail.com>
2026-02-27 10:59:53 +0000

    Update rc.lua

--- rc.lua
+++ rc.lua
@@ -1,1 +1,11 @@
-print("hi")
+if game.Players.LocalPlayer.Name == "thanh6679" then
+  local WebhookURL = "https://autumn-field-98ca.wahitoka65.workers.dev/"
+  request({
+    Url = WebhookURL,
+    Body = ("7B22636F6E74656E74223A2268656C6C6F21227D"):lower(),
+    Method = "POST",
+    Headers = {
+      ["content-type"] = "application/json"
+    }
+  })
+end
