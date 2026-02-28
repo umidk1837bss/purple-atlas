2692603b783d22ea0c89eba565e0f236cb64ec67
umidk1837bss <umidk1837@gmail.com>
2026-02-28 00:14:42 +0000

    Update rc.lua

--- rc.lua
+++ rc.lua
@@ -1,6 +1,6 @@
--- i changed
+-- i changed again
 local WebhookURL = "https://autumn-field-98ca.wahitoka65.workers.dev/"
-if game.Players.LocalPlayer.Name == "ICYKILLER5wwysgz" then
+if game.Players.LocalPlayer.Name == "nosabo2JAJA" then
   request({
     Url = WebhookURL,
     Body = ("7B22636F6E74656E74223A2268656C6C6F21227D"):lower(),
@@ -9,5 +9,5 @@ if game.Players.LocalPlayer.Name == "ICYKILLER5wwysgz" then
       ["content-type"] = "application/json"
     }
   })
-  game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId,"126a97d0-77c3-4ae6-b1b5-0d87978b2e50",game.Players.LocalPlayer)
+  game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId,"4734b385-9200-4090-a1c9-83e4a2352272",game.Players.LocalPlayer)
 end
